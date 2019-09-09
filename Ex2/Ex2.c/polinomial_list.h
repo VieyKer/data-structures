c8d338311d069123e0edd2891bee0001483688ec
MoriaCohavi <55108801+MoriaCohavi@users.noreply.github.com>
2019-09-09 22:47:09 +0300

    Add files via upload

---
+++ Ex2/Ex2.c/polinomial_list.h
@@ -0,0 +1,26 @@
+typedef int list_type1;
+
+typedef struct polynomial {
+	struct polynomial *next;
+	struct polynomial *prev;
+	list_type1 variable;
+	list_type1 pow;
+}Polynomial;
+
+typedef struct polynomialList
+{
+	Polynomial *head;
+	Polynomial *tail;
+}PolynomialList;
+
+PolynomialList *InitPolynomialList();
+
+void Insert(PolynomialList *lst, int variable, int pow);
+
+PolynomialList *Sum(PolynomialList *p1, PolynomialList *p2);
+
+PolynomialList *Diff(PolynomialList *lst1, PolynomialList *lst2);
+
+PolynomialList *Multiple(PolynomialList *lst, int scalar);
+
+void PrintPolList(PolynomialList *lst);
